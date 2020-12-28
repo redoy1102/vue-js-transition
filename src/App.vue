d3670fa971f61996f8a7804fe163960ef16ecfbe
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2020-12-29 00:06:48 +0600

    Transition added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <button class="btn btn primary" >Check</button>
+  <div class="container">
+    <transition name="appear"
+    enter-active-class="animated slideInDown"
+    leave-active-class="animated slideInUp">
+
+      <div class="p-3 mb-2 bg-success text-white" v-if="display" >Hello World</div>
+
+    </transition>
+
+    <button @click="display=!display" class="btn btn-primary" >Toggle</button>
   </div>
 </template>
+
+
+<script>
+export default  {
+  data(){
+    return{
+      display: false
+    }
+  }
+}
+</script>>
